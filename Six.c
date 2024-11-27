61fa44757e47b590ce4be929f015868a29ea8b45
kumaryash459 <iaskumaryash@gmail.com>
2024-11-28 00:08:39 +0530

    prm6

--- Six.c
+++ Six.c
@@ -1,3 +1,10 @@
+// Develop a menu driven Program in C for the following operations on Circular QUEUE of
+// Characters (Array Implementation of Queue with maximum size MAX)
+// a. Insert an Element onto Circular QUEUE
+// b. Delete an Element from Circular QUEUE
+// c. Demonstrate Overflow and Underflow situations on Circular QUEUE
+// d. Display the status of Circular QUEUE
+// e. Exit
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 5
