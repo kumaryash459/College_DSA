f6c57af2e56085cc2b33a21e434f5ba7ef5a54d7
kumaryash459 <iaskumaryash@gmail.com>
2024-12-03 21:18:21 +0530

    added question in pgm10

--- OOPS/pgm9.cpp
+++ OOPS/pgm9.cpp
@@ -1,4 +1,4 @@
-// develop a cpp program to create a text file , check file created or not , if created it will write some text int the file adn then read the text from the file
+// develop a cpp program to create a text file , check file created or not , if created it will write some text int the file adn then read the text from the file.
 
 #include <iostream>
 #include <conio.h>
@@ -16,11 +16,12 @@ int main()
     outFile<<"This is another line in the file";
     outFile.close();
     ifstream inFile("sample.txt");
-    if(!inFile){
+    if(!inFile)
+    {
         cout<<"Error opening the file"<<endl;
         return 1;
     }
-    cout<<"contents if the file:"<<endl;
+    cout<<"contents of the file:"<<endl;
     char ch;
     inFile.close();
     return 0;
